e4d435dc91a021f0baa052c53907d8546632c543
wiederm <marcus.wieder@univie.ac.at>
2019-08-14 01:44:14 +0200

    fixed bug - initale sampling of starting conformation now NOT added to trajectory.

--- neutromeratio/mcmc.py
+++ neutromeratio/mcmc.py
@@ -216,7 +216,7 @@ class NonequilibriumMC(MC_Mover):
     def performe_md_mc_protocoll(self,
                                 x0:unit.quantity.Quantity,
                                 nr_of_mc_trials:int = 500,
-                                nr_of_md_steps:int = 50
+                                nr_of_md_steps:int = 20
                                 ):
         """
         """    
