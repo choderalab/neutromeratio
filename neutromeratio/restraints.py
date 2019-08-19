b6660275dac87712175836cdfca365972035e689
wiederm <marcus.wieder@univie.ac.at>
2019-08-19 18:00:17 +0200

    harmonic bias finished.

--- neutromeratio/restraints.py
+++ neutromeratio/restraints.py
@@ -6,6 +6,7 @@ from .constants import bond_length_dict
 import logging
 from scipy.stats import norm
 from torch.distributions.normal import Normal
+from openmmtools.constants import kB
 
 logger = logging.getLogger(__name__)
 
@@ -91,18 +92,16 @@ def harmonic_position_restraint(x, tautomer_transformation:dict, atom_list:list,
     else:
         raise RuntimeError('Something went wrong.')
     
-
-    k = 1
+    sigma = 0.1 * unit.angstrom
+    T = 300 * unit.kelvin
+    k = (kB * T) / (sigma**2) 
     heavy_atom_element = atom_list[heavy_atom_idx]
     mean_bond_length = (bond_length_dict['{}H'.format(heavy_atom_element)]).value_in_unit(unit.angstrom)
     logging.debug('Mean bond length: {}'.format(mean_bond_length))
 
     distance = torch.norm(x[0][tautomer_transformation['hydrogen_idx']] - x[0][heavy_atom_idx]) * nm_to_angstroms
     logging.debug('Distance: {}'.format(distance))
-    
-
-    n =  Normal(loc=torch.tensor([mean_bond_length]).double(), scale=torch.tensor([0.1]).double())
-    e = - (k * n.log_prob(distance.double()).exp())
-   
+    k = k.value_in_unit((unit.kilo * unit.joule) / ((unit.angstrom **2) * unit.mole))
+    e = (k/2) *(distance.double() - mean_bond_length)**2
     logging.debug('Harmonic bias introduced: {:0.4f}'.format(e.item()))
     return e
