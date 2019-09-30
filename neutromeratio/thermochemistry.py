83c6fdad8e38e11fba6a4c83fe064d5e13a4f1ca
wiederm <marcus.wieder@univie.ac.at>
2019-09-30 15:37:26 +0200

    importing temp from constants

--- neutromeratio/thermochemistry.py
+++ neutromeratio/thermochemistry.py
@@ -2,7 +2,7 @@ import ase
 from ase.vibrations import Vibrations
 from ase.thermochemistry import IdealGasThermo
 from simtk import unit
-
+from constants import temperature
 
 def get_thermo_correction(ase_mol:ase.Atoms) -> simtk.unit.quantity.Quantity :
     """
@@ -23,7 +23,8 @@ def get_thermo_correction(ase_mol:ase.Atoms) -> simtk.unit.quantity.Quantity :
                             atoms=ase_mol,
                             geometry='nonlinear',
                             symmetrynumber=1, spin=0)
-    G = thermo.get_gibbs_energy(temperature=298.15, pressure=101325.)
+    
+    G = thermo.get_gibbs_energy(temperature=temperature.value_in_unit(unit.kelvin), pressure=101325.)
 
     return (G * 96.485) * unit.kilojoule_per_mole # eV * conversion_factor(eV to kJ/mol)
 
