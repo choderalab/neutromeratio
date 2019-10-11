91479f19d3f1343cdcf9d0c0cd2ea538c86c6062
wiederm <marcus.wieder@univie.ac.at>
2019-10-11 03:18:28 +0200

    bugfix

--- scripts/submit_generate_mm_energy.sh
+++ scripts/submit_generate_mm_energy.sh
@@ -7,14 +7,10 @@
 #$ -o /data/shared/projects/SGE_LOG/
 ￼
 molecule_name=$1 
-lambda_value=$2
-n_steps=50000
 
 cd /home/mwieder/Work/Projects/neutromeratio/scripts
 hostname
 echo ${molecule_name}
-echo ${lambda_value}
-echo ${n_steps}
 
 . /data/shared/software/python_env/anaconda3/etc/profile.d/conda.sh
 conda activate ani36
