14102c2abdd24f6ad84d83132883fb0d46587cbf
wiederm <marcus.wieder@univie.ac.at>
2019-10-21 17:32:48 +0200

    bugfix

--- scripts/bfe_submit_generate_equilibrium_sampling_in_droplet.sh
+++ scripts/bfe_submit_generate_equilibrium_sampling_in_droplet.sh
@@ -13,12 +13,12 @@
 idx=$1 
 n_steps=50000 
 
-cd /home/mwieder/Work/Projects/neutromeratio/scripts
+cd /home/wiederm/neutromeratio/neutromeratio/scripts
 hostname
 echo ${idx}
 echo ${n_steps}
 
-. /data/shared/software/python_env/anaconda3/etc/profile.d/conda.sh
+. /home/wiederm/anaconda3/etc/profile.d/conda.sh
 conda activate ani36
 #mkdir -p ../data/equilibrium_sampling/${molecule_name}
 python BFE_Generate_equilibrium_sampling_in_droplet.py ${idx} ${n_steps}
