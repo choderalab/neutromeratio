a58ad18b07319f7d1060dbc7a4ef360ce392470e
wiederm <marcus.wieder@univie.ac.at>
2019-10-24 14:18:46 +0200

    avoiding racetime condition

--- scripts/BFE_Generate_equilibrium_sampling_in_droplet.py
+++ scripts/BFE_Generate_equilibrium_sampling_in_droplet.py
@@ -88,9 +88,7 @@ langevin = neutromeratio.LangevinDynamics(atoms = tautomer.ligand_in_water_atoms
 x0 = np.array(tautomer.ligand_in_water_coordinates) * unit.angstrom
 #x0 = energy_function.minimize(x0) # NOTE: No minimizing!
 
-if not os.path.exists(f"/data/chodera/wiederm/equilibrium_sampling/{name}/"):
-    os.makedirs(f"/data/chodera/wiederm/equilibrium_sampling/{name}/")
-
+os.makedirs(f"/data/chodera/wiederm/equilibrium_sampling/{name}/", exist_ok=True)
 equilibrium_samples, energies, bias = langevin.run_dynamics(x0, n_steps=n_steps, stepsize=0.5 * unit.femtosecond, progress_bar=False)
     
 
--- scripts/bfe_submit_generate_equilibrium_sampling_in_droplet.sh
+++ scripts/bfe_submit_generate_equilibrium_sampling_in_droplet.sh
@@ -5,7 +5,7 @@
 #BSUB -R span[ptile=2]
 #BSUB -o /home/wiederm/LOG/equ_droplet_job%J.log
 #BSUB -L /bin/bash
-#BSUB -R rusage[mem=2]
+#BSUB -R rusage[mem=3]
 
 idx=$LSB_JOBINDEX 
 n_steps=50000 
