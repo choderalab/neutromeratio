c818426c77532711df4b8324d46fca6eab666fdb
wiederm <marcus.wieder@univie.ac.at>
2019-11-27 18:49:43 +0100

    used to generate mbar estimates excluding conformations that include very high stddev (but using trajectories that have confs with high stddev)

--- scripts/bfe_submit_analyse_equilibrium_samples.sh
+++ scripts/bfe_submit_analyse_equilibrium_samples.sh
@@ -9,13 +9,13 @@
 
 idx=$LSB_JOBINDEX
 hostname
-per_atom_stddev_treshold=0.5
+per_atom_stddev_threshold=0.5
 diameter_in_angstrom=18 #Angstrom
 base_path="/data/chodera/wiederm/equilibrium_sampling/waterbox-${diameter_in_angstrom}A/"
 env='droplet'
 
 echo 'Idx: '${idx}
-echo 'Per atom stddev treshold: '${per_atom_stddev_treshold}
+echo 'Per atom stddev threshold: '${per_atom_stddev_threshold}
 echo 'Diameter in Angstrom: '${diameter_in_angstrom}
 echo 'Base path: '${base_path}
 
@@ -23,4 +23,4 @@ echo 'Base path: '${base_path}
 conda activate ani36
 # nr of jobs: 500
 
-python Analyse_equilibrium_samples_in_droplet.py ${idx} ${base_path} ${env} ${per_atom_stddev_treshold} ${diameter_in_angstrom}
+python Analyse_equilibrium_samples_in_droplet.py ${idx} ${base_path} ${env} ${per_atom_stddev_threshold} ${diameter_in_angstrom}
