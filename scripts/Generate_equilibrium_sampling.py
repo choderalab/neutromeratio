cbc34a17f29fa87fd8e60b3f2928d633a143ef9e
wiederm <marcus.wieder@univie.ac.at>
2020-01-22 18:51:04 +0100

    single equilibrium sampling script

--- scripts/Generate_equilibrium_sampling.py
+++ scripts/Generate_equilibrium_sampling.py
@@ -21,6 +21,13 @@ idx = int(sys.argv[1])
 n_steps = int(sys.argv[2])
 # where to write the results
 base_path = str(sys.argv[3])
+# env
+env = str(sys.argv[4])
+assert(env == 'droplet' or env == 'vacuum')
+
+# diameter
+if env == 'droplet':
+    diameter_in_angstrom = int(sys.argv[5])
 
 protocol = []
 exp_results = pickle.load(open('../data/exp_results.pickle', 'rb'))
@@ -28,7 +35,7 @@ for name in sorted(exp_results):
     if name in exclude_set:
         continue
     for lamb in np.linspace(0, 1, 11):
-        protocol.append((name, lamb))
+        protocol.append((name, np.round(lamb, 2)))
 
 name, lambda_value = protocol[idx-1]
 print(name)
@@ -45,17 +52,24 @@ kappa_value = 0.0
 for tautomer in tautomers:
     print(kappa_value)
     tautomer.perform_tautomer_transformation()
-    pdb_filepath = f"{base_path}/{name}/{name}_{kappa_value}.pdb"
 
-    try:
-        traj = md.load(pdb_filepath)
-    except OSError:
-        coordinates = tautomer.hybrid_coords
-        traj = md.Trajectory(coordinates.value_in_unit(unit.nanometer), tautomer.hybrid_topology)
-        traj.save_pdb(pdb_filepath)
-
-    # set coordinates #NOTE: note the xyz[0]
-    tautomer.hybrid_coords = traj.xyz[0] * unit.nanometer
+    if env == 'droplet':
+        m = tautomer.add_droplet(tautomer.hybrid_topology, 
+                            tautomer.hybrid_coords, 
+                            diameter=diameter_in_angstrom * unit.angstrom,
+                            restrain_hydrogen_bonds=True,
+                            restrain_hydrogen_angles=False,
+                            file=f"{base_path}/{name}/{name}_in_droplet_{mode}.pdb")
+    else:
+        pdb_filepath = f"{base_path}/{name}/{name}_{kappa_value}.pdb"
+        try:
+            traj = md.load(pdb_filepath)
+        except OSError:
+            coordinates = tautomer.hybrid_coords
+            traj = md.Trajectory(coordinates.value_in_unit(unit.nanometer), tautomer.hybrid_topology)
+            traj.save_pdb(pdb_filepath)
+        # set coordinates #NOTE: note the xyz[0]
+        tautomer.hybrid_coords = traj.xyz[0] * unit.nanometer
 
     # define the alchemical atoms
     alchemical_atoms = [tautomer.hybrid_hydrogen_idx_at_lambda_1, tautomer.hybrid_hydrogen_idx_at_lambda_0]
@@ -78,10 +92,25 @@ for tautomer in tautomers:
     for r in tautomer.hybrid_ligand_restraints:
         energy_function.add_restraint_to_lambda_protocol(r)
 
+    if env == 'droplet':
+        tautomer.add_COM_for_hybrid_ligand(np.array([diameter_in_angstrom/2, diameter_in_angstrom/2, diameter_in_angstrom/2]) * unit.angstrom)
+
+        for r in tautomer.solvent_restraints:
+            energy_function.add_restraint_to_lambda_protocol(r)
+
+        for r in tautomer.com_restraints:
+            energy_function.add_restraint_to_lambda_protocol(r)
+
+
     energy_and_force = lambda x : energy_function.calculate_force(x, lambda_value)
     langevin = neutromeratio.LangevinDynamics(atoms=tautomer.hybrid_atoms,
                                             energy_and_force=energy_and_force)
-    x0 = tautomer.hybrid_coords
+    if env == 'droplet':
+        x0 = tautomer.ligand_in_water_coordinates
+    elif env == 'vacuum':
+        x0 = tautomer.hybrid_coords
+    else:
+        raise RuntimeError()
     x0, e_history = energy_function.minimize(x0, maxiter=5000, lambda_value=lambda_value)
 
     equilibrium_samples, energies, restraint_bias, stddev, ensemble_bias = langevin.run_dynamics(x0,
@@ -90,28 +119,16 @@ for tautomer in tautomers:
                                                                         progress_bar=False)
 
     # save equilibrium energy values
-    f = open(f"{base_path}/{name}/{name}_lambda_{lambda_value:0.4f}_kappa_{kappa_value:0.4f}_energy_in_vacuum.csv", 'w+')
-    for e in energies[::20]:
-        e_unitless = e / kT
-        f.write('{}\n'.format(e_unitless))
-    f.close()
-
-    # save stddev energy values
-    f = open(f"{base_path}/{name}/{name}_lambda_{lambda_value:0.4f}_kappa_{kappa_value:0.4f}_stddev_in_vacuum.csv", 'w+')
-    for e in stddev[::20]:
-        e_unitless = e / kT
-        f.write('{}\n'.format(e_unitless))
-    f.close()
-
-    # save restraint bias values
-    f = open(f"{base_path}/{name}/{name}_lambda_{lambda_value:0.4f}_kappa_{kappa_value:0.4f}_restraint_bias_in_vacuum.csv", 'w+')
-    for e in ensemble_bias[::20]:
-        e_unitless = e / kT
-        f.write('{}\n'.format(e_unitless))
-    f.close()
+    # save equilibrium energy values 
+    for global_list, poperty_name in zip([energies, stddev, restraint_bias, ensemble_bias], ['energy', 'stddev', 'restraint_bias', 'ensemble_bias']):
+        f = open(f"{base_path}/{name}/{name}_lambda_{lambda_value:0.4f}_{poperty_name}_in_{env}.csv", 'w+')
+        for e in global_list[::20]:
+            e_unitless = e / kT
+            f.write('{}\n'.format(e_unitless))
+        f.close()   
 
     equilibrium_samples = [x.value_in_unit(unit.nanometer) for x in equilibrium_samples]
     ani_traj = md.Trajectory(equilibrium_samples[::20], tautomer.hybrid_topology)
-    ani_traj.save(f"{base_path}/{name}/{name}_lambda_{lambda_value:0.4f}_kappa_{kappa_value:0.4f}_in_vacuum.dcd", force_overwrite=True)
+    ani_traj.save(f"{base_path}/{name}/{name}_lambda_{lambda_value:0.4f}_kappa_{kappa_value:0.4f}_in_{env}.dcd", force_overwrite=True)
 
     kappa_value += 1.0
