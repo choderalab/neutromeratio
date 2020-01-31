eed4f372b81307af57837809f0495c9491685e9c
wiederm <marcus.wieder@univie.ac.at>
2020-01-31 16:42:51 +0100

    updated thin dcd script

--- scripts/SGE_Thin_dcd.py
+++ scripts/SGE_Thin_dcd.py
@@ -7,41 +7,29 @@ import mdtraj as md
 import matplotlib.pyplot as plt
 import sys
 import torch
-from neutromeratio.parameter_gradients import FreeEnergyCalculator
+from neutromeratio.constants import exclude_set
 
-#######################
+idx = int(sys.argv[1])
 # read in exp results, smiles and names
 exp_results = pickle.load(open('../data/exp_results.pickle', 'rb'))
 
 # name of the system
-name = str(sys.argv[1])
+protocoll = []
+for name in sorted(exp_results):
+    if name in exclude_set:
+        continue
+    protocoll.append(name)
 
-# don't change - direction is fixed for all runs
-#################
-from_mol_tautomer_idx = 1
-to_mol_tautomer_idx = 2
+name = protocoll[idx-1]
+print(name)
 
-t1_smiles = exp_results[name]['t1-smiles']
-t2_smiles = exp_results[name]['t2-smiles']
-
-# generate both rdkit mol
-mols = { 't1' : neutromeratio.generate_rdkit_mol(t1_smiles), 't2' : neutromeratio.generate_rdkit_mol(t2_smiles) }
-from_mol = mols[f"t{from_mol_tautomer_idx}"]
-to_mol = mols[f"t{to_mol_tautomer_idx}"]
-ani_input = neutromeratio.from_mol_to_ani_input(from_mol, nr_of_conf=1)
-
-# get tautomer transformation
-tautomer_transformation = neutromeratio.get_tautomer_transformation(from_mol, to_mol)
-neutromeratio.generate_hybrid_structure(ani_input, tautomer_transformation)
-
-# 20 steps inclusive endpoints
-# getting all the energies, snapshots and lambda values in lists
-# NOTE: This will be changed in the future
+base = '/data/shared/projects/neutromeratio/data/equilibrium_sampling/waterbox-18A/'
+pdb_file = f"{base}/{name}/{name}_in_droplet_forward.pdb"
 lambda_value = 0.0
 for _ in range(21):
-    f_traj = f"/home/mwieder/Work/Projects/neutromeratio/data/equilibrium_sampling/{name}/{name}_lambda_{lambda_value:0.4f}.dcd"
-    traj = md.load_dcd(f_traj, top=ani_input['hybrid_topology'])
-    traj[::10].save(f"../data/equilibrium_sampling/{name}/{name}_lambda_{lambda_value:0.4f}_thinned.dcd", force_overwrite=True)
+    f_traj = f"{name}/{name}_lambda_{lambda_value:0.4f}.dcd"
+    traj = md.load_dcd(f_traj, top=pdb_file)
+    traj[::10].save(f"{base}/{name}/{name}_lambda_{lambda_value:0.4f}_thinned.dcd", force_overwrite=True)
     lambda_value +=0.05
 
 
