d4f50bbb8e1cd70c1997c34db0ef4f298359bf1f
wiederm <marcus.wieder@univie.ac.at>
2020-04-29 20:06:31 +0200

    updating test

--- neutromeratio/tests/test_neutromeratio.py
+++ neutromeratio/tests/test_neutromeratio.py
@@ -942,5 +942,10 @@ def test_parameter_gradient():
 
     deltaF.backward()  # no errors or warnings
     params = list(energy_function.model.parameters())
+    non_counter = 0
     for p in params:
-        assert(p.grad == None)  # all None
+        if(p.grad == None):  # some are None!
+            non_counter += 1
+
+    assert(len(params) == 256)
+    assert(non_counter == 64)
