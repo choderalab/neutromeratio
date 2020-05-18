efb0f5f1a0b2e255ba56fbc55f5d1d440e3af782
wiederm <marcus.wieder@univie.ac.at>
2020-05-18 20:37:14 +0200

    parameter_gradien-opt.py update

--- scripts/parameter_gradient-opt.py
+++ scripts/parameter_gradient-opt.py
@@ -2,10 +2,16 @@ import neutromeratio
 import pickle
 import pkg_resources
 from neutromeratio.parameter_gradients import tweak_parameters
-
+from neutromeratio.constants import exclude_set_ANI, mols_with_charge
 
 data = pkg_resources.resource_stream(neutromeratio.__name__, "data/exp_results.pickle")
 print(f"data-filename: {data}")
 exp_results = pickle.load(data)
-names = [name for name in exp_results][:20] 
-tweak_parameters(data_path='./data', nr_of_nn=8, names = ['SAMPLmol2', 'SAMPLmol4'], max_epochs=100)
+
+names = []
+for name in exp_results:
+    if name in exclude_set_ANI + mols_with_charge:
+        continue
+        names.appen(name)
+
+tweak_parameters(data_path='./data', nr_of_nn=8, names = names[:30], max_epochs=100)
